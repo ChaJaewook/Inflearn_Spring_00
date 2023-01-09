8792062e1654da61eec3907bc7b60d8fa21efc00
ChaJaewook <44600628+ChaJaewook@users.noreply.github.com>
2023-01-10 00:04:27 +0900

    24강. 스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,30 +8,39 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //AppConfig : 애플리케이션의 전체 동작 방식을 구성(config)하기위해 구현객체를 생성하고,
 // "연결"하는 책임을 가지는 별도의 설정 클래스를 만들자
 // 애플리케이션의 실제 동작에 필요한 "구현 객체를 생성"한다.
 // 생성한 객체 인스턴스의 참조(레퍼런스)를 "생성자를 통해서 주입(연결)"해준다.
+
+//어플리케이션의 구성(설정) 정보
+// 설정정보에는 @Cofiguration을 적어준다.
+@Configuration
 public class AppConfig {
 
     //역할들이 잘 들어나게 리팩토링이 필요
+    @Bean
     public MemberService memberService()
     {
         //생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService()
     {
         //return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         //AppConfig에서 할인 정책 역할을 담당하는 구현을 변경했다.
         //이제 할인정책인 변경돼도 애플리케이션의 구성역할을 담당하는 AppConfig만 변경하면 된다.
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,16 +4,28 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     //순수한 JAVA코드
     public static void main(String[] args) {
-        AppConfig appConfig=new AppConfig();
-
-
-        MemberService memberService=appConfig.memberService();
+//        AppConfig appConfig=new AppConfig();
+//
+//
+//        MemberService memberService=appConfig.memberService();
         //MemberService memberService=new MemberServiceImpl(memberRepository);
+
+        //스프링 생성
+        // 앞서 선언한 @Bean을 관리해준다.
+        // parameter로 AppConfig를 넣어준다.
+        // AppConfig의 환경정보를 갖고 Spring Container에서 관리해준다.
+        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //첫번째 파라미터에는 메소드 이름, 두번째 메소드에는 반환받을 타입을 입력
+        MemberService memberService=applicationContext.getBean("memberService",MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
