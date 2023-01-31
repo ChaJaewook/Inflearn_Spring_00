de3207c2e35d33e7aefd4a7922de20e58e8eaac7
ChaJaewook <44600628+ChaJaewook@users.noreply.github.com>
2023-02-01 00:04:57 +0900

    - 스코프와 Provider  - 스코프와 프록시

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -9,10 +10,21 @@ import java.util.UUID;
 //동시에 고객 요청이 오면 log가 섞일 수 있다.
 //request scope를 사용하자!
 
+//Provider와 프록시 둘다 핵심아이디어는 "진짜 객체 조회를 꼭 필요한 시점까지 지연처리한다는 점이다."
+
 @Component
-@Scope(value="request")
+@Scope(value="request",proxyMode= ScopedProxyMode.TARGET_CLASS)
+//proxy mdoe라는 parameter 추가
+//프록시를 통해 가짜를 만든다.
+//프록시 모드
+// 적용 대상이 인터페이스 아닌 class면 Target_Class
+// 적용 대상이 인터페이스면 interfaces를 주면 된다.
+// 가짜 프록시 클래스를 만들고 미리 http를 주입한다.
+//$$EnhancerBySpringCGLIB$$2699f515 이런 가짜 프록시 개체를 등록한다.
+
 public class MyLogger {
 
+    //여러 요청이 와도 객체를 각각 다룬다는데 핵심이 있다.
     private String uuid;
     private String requestURL;
 
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,15 +15,20 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
     private final LogDemoService logDemoService;
     private final MyLogger myLogger;
-
+    //ObjectProvider로 수정
+    //MyLogger를 찾을 수 있는 dependency
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
+    //Request Scope의 빈 생성을 지연
     @RequestMapping("log-demo")
     @ResponseBody
     //문자를 그대로 응답을 보낼 수 있다.
     public String logDemo(HttpServletRequest request) //고객 요청정보를 확인 할 수 있다.
     {
+        //MyLogger myLogger=myLoggerProvider.getObject();
         //고객이 요청할 URL을 알 수 있다.
         String requestURL=request.getRequestURL().toString();
 
+        System.out.println("myLogger = " + myLogger.getClass());
         //URL Setting
         myLogger.setRequestURL(requestURL);
 
